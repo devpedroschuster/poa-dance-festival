cefe197afc238e004699958c0ce75a5697b26902
pedrorschuster <pedrorschuster@gmail.com>
2026-01-12 10:16:44 -0300

    fix:mudando porta email para 587

--- server/server.js
+++ server/server.js
@@ -31,14 +31,15 @@ const upload = multer({ storage: storage });
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
-  connectionTimeout: 10000, 
-  greetingTimeout: 10000 
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 mongoose.connect(process.env.MONGO_URI)
